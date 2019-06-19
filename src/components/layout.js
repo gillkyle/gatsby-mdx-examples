74f1dcf6c8e2dfd4bbf54c9f7bc9befe3865d66f
gillkyle <kylerobertgill@gmail.com>
2019-06-19 12:07:31 -0600

    add live example

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { MDXProvider } from "@mdx-js/tag";
+import Header from "./header";
+import "./layout.css";
 
-import Header from './header'
-import './layout.css'
+const MyCodeComponent = ({ children, ...props }) => (
+  <LiveProvider code={children}>
+    <LiveEditor />
+    <LiveError />
+    <LivePreview />
+  </LiveProvider>
+);
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,24 +27,26 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <MDXProvider components={{ code: MyCodeComponent }}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: "0 auto",
+              maxWidth: 960,
+              padding: "0px 1.0875rem 1.45rem",
+              paddingTop: 0
+            }}
+          >
+            {children}
+          </div>
+        </MDXProvider>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
